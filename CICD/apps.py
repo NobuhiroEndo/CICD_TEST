8047753084da8febb18798915ad94dbe4e8d2e5d
februar-END <nobuhiro.0320@gmail.com>
2024-04-29 23:45:07 +0900

    add black

--- CICD/apps.py
+++ CICD/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class CicdConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'CICD'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "CICD"
